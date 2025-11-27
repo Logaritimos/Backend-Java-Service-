897bda83bd71ff546a595b481583fc7c047347f2
Erick David da Silva <erick.silva097@sptech.school>
2025-11-27 18:24:50 -0300

    feat: padronizacao e get bucket name

--- log_aritmos/src/main/java/school/sptech/S3Reader.java
+++ log_aritmos/src/main/java/school/sptech/S3Reader.java
@@ -1,16 +1,29 @@
 package school.sptech;
 
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class S3Reader {
 
     public InputStream getFileFromS3(String bucketName, String key) {
 
         S3Client s3 = new S3Provider().getS3Client();
+        List<Bucket> buckets = s3.listBuckets().buckets();
+        for (Bucket bucketDaVez : buckets) {
+            if(bucketName.contains(bucketDaVez.name())){
+                bucketName = bucketDaVez.name();
+                GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .build();
 
+                return s3.getObject(getObjectRequest);
+            }
+        }
         GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
